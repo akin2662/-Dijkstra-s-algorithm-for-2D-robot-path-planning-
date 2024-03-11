d3dba813b89f1776313acdf4d8de156b5923b5fb
akin2662 <162997666+akin2662@users.noreply.github.com>
2024-03-11 11:35:18 -0400

    Tested Djikstra Algorithm
    Included performActions so that it would be easier to get the neighbors in the algorithm, added backtracking

--- djikstra_point_robot.py
+++ djikstra_point_robot.py
@@ -1,6 +1,9 @@
 # importing important libraries
 import cv2 as cv
 import numpy as np
+from collections import OrderedDict
+import heapq as hq
+import time
 
 
 # Generating the map
@@ -101,6 +104,57 @@ def down_left(image, node):
     if within_boundary(new_node) and new_node in in_free_space(image):
         return (new_node,np.sqrt(2))
 
+def performActions(image,node):
+    u = up(image, node)
+    d = down(image, node)
+    l = left(image, node)
+    r = right(image, node)
+    ur = up_right(image,node)
+    ul = up_left(image,node)
+    dr = down_right(image,node)
+    dl = down_left(image,node)
+
+    return [u,d,l,r,ur,ul,dr,dl]
+
+def backtrack(path,start_node,goal_node):
+    node = goal_node
+    final_path = []
+    while node is not start_node:
+        final_path.append(node)
+        node = path[node]
+    final_path.append(start_node)
+    final_path.reverse()
+    return final_path
+
+def djikstra(image,start_node,goal_node):
+    start_node_cost = 0
+    open_list = []
+    closed_list = {start_node:start_node_cost}
+    path = {}
+    hq.heappush(open_list,(start_node_cost,start_node))
+    hq.heapify(open_list)
+    while open_list:
+        current_distance, current_node = hq.heappop(open_list)
+        if current_node == goal_node and current_node != start_node:
+            break
+
+        neighbors = performActions(image,current_node)
+
+        for neighbor in neighbors:
+            if neighbor is not None:
+                new_node, new_cost = neighbor
+                new_distance = current_distance + new_cost
+
+                if new_node not in closed_list or new_distance< closed_list[new_node] :
+                    closed_list[new_node] = new_distance
+                    path[new_node] = current_node
+                    hq.heappush(open_list, (closed_list[new_node], new_node))
+
+    final_path = backtrack(path,start_node,goal_node)
+    return final_path
+
+
+start_time = time.time()
 image = map()
 start_y = int(input("Y coordinate of the start position:"))
 start_x = int(input("X coordinate of the start position:"))
@@ -109,6 +163,9 @@ start_node = (start_y,start_x)
 goal_y = int(input("Y coordinate of the goal position"))
 goal_x = int(input("X coordinate of the goal position"))
 goal_node = (goal_y,goal_x)
-# cv.imshow("map",image)
-# cv.waitKey(0)
+
+path = djikstra(image,start_node,goal_node)
+print(path)
+end_time = time.time()
+print(end_time-start_time)
 
